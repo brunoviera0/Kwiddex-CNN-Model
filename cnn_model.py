eea8cebd86330cb1bacd6fce9da028c5dba8174d
nameera-n <nameera.n40@gmail.com>
2025-11-17 23:05:07 +0000

    remove weights from model

--- cnn_model.py
+++ cnn_model.py
@@ -65,7 +65,8 @@ def main():
     # Class weights to compensate for dataset imbalance
     # Real ≈ 825, Fake ≈ 163 ⇒ ~5:1 ratio
     # Class weights = [1.0, 5.0] so mistakes on fake class are penalized more
-    # class_weights = torch.tensor([1.0, 5.0], dtype=torch.float32).to(DEVICE)
+    #  class_weights = torch.tensor([1.0, 5.0], dtype=torch.float32).to(DEVICE)
+>>>>>>> 9b7d0ff (remove weights from model)
     criterion = nn.CrossEntropyLoss()
 
     best_acc = 0.0
@@ -130,3 +131,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
